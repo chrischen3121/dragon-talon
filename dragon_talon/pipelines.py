281b10379a5bd130030fe0c8eff83b01a7a559c9
chrischen <chrischen3121@gmail.com>
2021-01-26 09:34:51 +0800

    updated mongo pipeline

--- dragon_talon/pipelines.py
+++ dragon_talon/pipelines.py
@@ -1,7 +1,9 @@
 import os
+from dataclasses import asdict
 from typing import Optional
 
 from itemadapter import ItemAdapter
+from motor import motor_asyncio
 from pymongo import ASCENDING, IndexModel, MongoClient
 
 from . import items
@@ -16,14 +18,12 @@ def _get_mongo_uri() -> str:
 
 
 class MongoPipeline:
-
-    collection_name = "scrapy_items"
-    # TODO: items to different collections
-
     def __init__(self):
         self._mongo_uri = _get_mongo_uri()
         self._dbname = "scrapy"
         self._colname: Optional[str] = None
+        self._motor_cli: Optional[motor_asyncio.AsyncIOMotorClient] = None
+        self._motor_col = None
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -34,20 +34,24 @@ class MongoPipeline:
             self._colname = "xiaoqu"
             cli = MongoClient(self._mongo_uri, tz_aware=True)
             db = cli.get_database(self._dbname)
-            col = db.get_collection("self._colname")
-            # index1 = IndexModel([("xiaoqu_id", ASCENDING), unique=True])
-            # col.create_indexes([index1])
-        # TODO: create motor cli
+            col = db.get_collection(self._colname)
+            index1 = IndexModel([("xiaoqu_id", ASCENDING)], unique=True)
+            index2 = IndexModel([("district", ASCENDING), ("area", ASCENDING)])
+            col.create_indexes([index1, index2])
+        else:
+            raise RuntimeError("unknown spider {repr(spider)}")
+
+        self._motor_cli = motor_asyncio.AsyncIOMotorClient(self._mongo_uri, tz_aware=True)
+        db = self._motor_cli.get_database(self._dbname)
+        self._motor_col = db.get_collection(self._colname)
 
     def close_spider(self, spider):
-        # self.client.close()
         pass
 
     async def process_item(self, item, spider):
+        record_to_insert = asdict(item)
+        # self._motor_col
+        print(record_to_insert)
         # if isinstance(item, items.XiaoquItem):
         #     return await self._process_xiaoqu_item(item)
-        # self.db[self.collection_name].insert_one(ItemAdapter(item).asdict())
-        pass
-
-    async def _process_xiaoqu_item(self, item):
         pass
